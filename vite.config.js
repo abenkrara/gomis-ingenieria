029dc038c96bf393fef2e61c438a521e7e9c80d8
Ayub Benkrara <abenkrara7@gmail.com>
2026-05-03 18:40:33 +0200

    chore: configurar despliegue en GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/gomis-ingenieria/',
   plugins: [
     react(),
     tailwindcss(),
